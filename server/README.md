c29ffb369977b21d4bd2cd4187058b9f18dc01d4
FrBreno <fbreno77@alu.ufc.br>
2022-12-24 09:36:55 -0300

    Stage 03 - Finalização do back-end

---
+++ server/README.md
@@ -0,0 +1,29 @@
+# Back-end
+
+## Entidades
+
+### Game
+
+id  
+title  
+bannerUrl
+
+### Ad
+
+id  
+gameId  
+name  
+yearsPlaying  
+discord  
+weekDays  
+hourStart  
+hourEnd  
+useVoiceChannel  
+createdAt
+
+## Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anuncios por game
+- Buscar discord pelo ID de anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,22 +2,107 @@
 // Primeira Forma ("type": "commonjs"):
 // const express = require('express')
 // Forma mais elegante ("type": "module"):
-import express from 'express'
+import express, { request, response } from 'express'
+import cors from 'cors'
+
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get('/ads', (request, response) => {
+const prisma = new PrismaClient()
+
+// Conhecimentos necessários:
+// 1. HTTP methods
+// 2. API RESTful
+// 3. HTTP Codes
+// 4. Parâmetros HTTP (Query, Route e Body)
+
+// Listagem de games com contagem de anúncios:
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count:{
+        select:{
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json([games])
+})
+
+// Criação de um novo anúncio:
+app.post('/games/:id/ads', async (request, response) =>{
+  const gameId = request.params.id;
+  const body: any = request.body
+
+  const ad = await prisma.ad.create({
+    data:{
+      gameId: gameId,
+      name: body.name,  
+      yearsPlaying: body.yearsPlaying,  
+      discord: body.discord,  
+      weekDays: body.weekDays.join(','),  
+      hourStart: convertHourStringToMinutes(body.hourStart),  
+      hourEnd: convertHourStringToMinutes(body.hourEnd),  
+      useVoiceChannel: body.useVoiceChannel
+    }
+  })
+  return response.status(201).json(ad)
+})
+
+// Listagem de anuncios por game:
+app.get('/games/:id/ads', async (request, response) => {
   //Request são as informações enviadas no acesso e response são as retornadas no acesso.
   // Vamos usar response para retornar um array de objetos via json:
-  return response.json([
-    { id: 1, name: 'Anúncio 1' },
-    { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' },
-    { id: 4, name: 'Anúncio 4' },
-    { id: 5, name: 'Anúncio 5' }
-  ])
+  const gameId = request.params.id
+  const ads = await prisma.ad.findMany({
+    select:{
+      id:true,
+      name:true,
+      weekDays:true,
+      useVoiceChannel:true,
+      yearsPlaying:true,
+      hourStart:true,
+      hourEnd:true
+    },
+    where:{
+      gameId: gameId
+    },
+    orderBy:{
+      createdAt:'desc'
+    }
+  })
+  return response.json([ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd)
+    }
+  })])
 }) //O primeiro parâmetro é o endereço de acesso e o segundo é uma função que irá "rodar" nesse endereço.
 
-app.listen(3333)
+// Buscar discord pelo ID de anúncio:
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord:true
+    },
+    where:{
+      id:adId
+    }
+  })
 
-// Aula 01 - 1:06:00
+  return response.json([{
+    discord: ad.discord
+  }])
+})
+
+app.listen(3333)

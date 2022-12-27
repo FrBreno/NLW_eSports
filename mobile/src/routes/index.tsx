f3722d61ba360a0494435daffef83ff0d49ed464
FrBreno <fbreno77@alu.ufc.br>
2022-12-27 15:52:28 -0300

    Stage 04 - Criação da tela de publicação de anúncios no front-end e das rotas 'home' e 'game' da aplicação mobile

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,10 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native"
+import { AppRoutes } from "./app.routes"
 
-import { AppRoutes } from "./app.routes";
-
-export function Routes() {
-  return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
-  );
+export function Routes(){
+    return(
+        <NavigationContainer>
+            <AppRoutes/>
+        </NavigationContainer>
+    )
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,30 +1,49 @@
-import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from 'react'
+import { Image, FlatList } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+import { styles } from './styles'
 
-import logoImg from '../../assets/logo-nlw-esports.png';
-import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
-
-import { GAMES } from '../../utils/games';
-import { styles } from './styles';
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading'
+import { Background } from '../../components/Background'
+import { GameCard, GameCardProps } from '../../components/GameCard'
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Image source={logoImg} style={styles.logo}/>
-      <Heading
-      title="Encontre seu duo!"
-      subtitle="Selecione o game que deseja jogar..."/>
 
-      <FlatList
-      data={GAMES}
-      keyExtractor={item => item.id}
-      renderItem={({item}) => (
-        <GameCard data={item}/>
-      )}
-      showsHorizontalScrollIndicator={false}
-      horizontal
-      contentContainerStyle={styles.contentList}
-      />
-    </View>
-  );
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  const navigation = useNavigation()
+
+  function handleOpenGame({id, title, bannerUrl}:GameCardProps){
+    navigation.navigate('game',{id, title, bannerUrl})
+  }
+
+  useEffect(() => {
+    fetch('http://192.168.0.179:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  },[])
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo}/>
+        <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."/>
+
+        <FlatList
+        data={games}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <GameCard data={item} onPress={() => handleOpenGame(item)}/>
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
+  )
 }
